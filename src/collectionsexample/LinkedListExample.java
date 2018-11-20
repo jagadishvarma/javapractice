00d51bcad43ee32167d5410a93a8ea48cc191b30
jagadish <jagadishvarma02@gmail.com>
2018-11-20 16:13:59 +0530

    added more files

--- src/collectionsexample/LinkedListExample.java
+++ src/collectionsexample/LinkedListExample.java
@@ -1,5 +1,6 @@
 package collectionsexample;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class LinkedListExample {
@@ -7,6 +8,7 @@ public class LinkedListExample {
 	public static void main(String []args) {
 		
 		LinkedList<String> ll = new LinkedList<String>();
+		LinkedList<Integer> lint = new LinkedList<Integer>();
 		
 		ll.add("varma");
 		ll.add("janu");
@@ -21,5 +23,21 @@ public class LinkedListExample {
 		
 		ll.remove("vijju");
 		System.out.println(ll);
+		
+		lint.add(1);
+		lint.add(2);
+		lint.add(3);
+		lint.add(4);
+		lint.add(4);
+		System.out.println(lint);
+		
+		lint.remove(2);
+		System.out.println(lint);
+		
+		lint.remove(Integer.valueOf("2"));
+		System.out.println(lint);
+		
+		lint.removeAll(Arrays.asList(4));
+		System.out.println(lint);
 	}
 }	
